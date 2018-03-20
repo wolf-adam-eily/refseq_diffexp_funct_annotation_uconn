57544fc5ee35cac6018048c398331b4a00b0f6cc
wolf-adam-eily <36998662+wolf-adam-eily@users.noreply.github.com>
2018-03-20 17:25:46 +0000

    Update and rename htseq_count_xanadu to htseq_count_xanadu.sh

--- htseq_count_xanadu.sh
+++ htseq_count_xanadu.sh
@@ -1,4 +1,14 @@
-#this file converts the bam files from this tutorial into counts files
+#!/bin/bash
+#SBATCH --job-name=htseq_count_xanadu
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 8
+#SBATCH --partition=general
+#SBATCH --mail-type=END
+#SBATCH --mail-user=
+#SBATCH --mem=50G
+#SBATCH -o htseq_count_xanadu_%j.out
+#SBATCH -e htseq_count_xanadu_%j.err
 module load htseq-count
 htseq-count -s no -r pos -t gene -i Dbxref -f bam sort_trim_LB2A_SRR1964642.bam GCF_000972845.1_L_crocea_1.0_genomic.gff > LB2A_SRR1964642.counts
 htseq-count -s no -r pos -t gene -i Dbxref -f bam sort_trim_LB2A_SRR1964643.bam GCF_000972845.1_L_crocea_1.0_genomic.gff > LB2A_SRR1964643.counts
